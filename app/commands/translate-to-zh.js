a35751cb0f2e11b65973f5d11f3e36492be1c900
Neil <cdcd71517@gmail.com>
2023-03-19 20:57:50 +0800

    Implement translate to zh (zh-tw)

---
+++ app/commands/translate-to-zh.js
@@ -0,0 +1,15 @@
+import { TYPE_TRANSLATE } from '../../constants/command.js';
+import { t } from '../../locales/index.js';
+import Command from './command.js';
+
+export default new Command({
+  type: TYPE_TRANSLATE,
+  label: t('__COMMAND_TRANSLATE_TO_ZH_LABEL'),
+  text: t('__COMMAND_TRANSLATE_TO_ZH_TEXT'),
+  prompt: t('__COMMAND_TRANSLATE_TO_ZH_PROMPT'),
+  aliases: [
+    '/translate-to-zh',
+    'Translate to Zhong Wen',
+    'Translate to ZH',
+  ],
+});
