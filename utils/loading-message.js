d7e483554c103b467b1c70a3b2267f5753fd10e8
cdcd72 <cdcd71517@gmail.com>
2024-07-13 18:51:14 +0800

    Modify line loading animation default wait 60s

--- utils/loading-message.js
+++ utils/loading-message.js
@@ -1,7 +1,7 @@
 import config from '../config/index.js';
 import { loading } from '../services/line.js';
 
-const loadingMessage = ({ chatId, loadingSeconds = 20 }) => {
+const loadingMessage = ({ chatId, loadingSeconds = 60 }) => {
   if (config.APP_ENV !== 'production') return { chatId, loadingSeconds };
   return loading({ chatId, loadingSeconds });
 };
